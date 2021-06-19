4e70ff7274e1a0128fc1e97779a7f99f9dccda67
Yusuke Asai <f.a.ysk.08@gmail.com>
2021-06-19 15:20:30 +0900

    first commit

---
+++ app/src/main/java/com/asai/favorite_phrase/utils/SwipeCotrollerActions.kt
@@ -0,0 +1,6 @@
+package com.asai.favorite_phrase.utils
+
+abstract class SwipeControllerActions {
+//        open fun onLeftClicked(position: Int) {}
+        open fun onRightClicked(position: Int) {}
+}
